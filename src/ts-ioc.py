77da0efe697c5d8f26317ddfd5ec07185603efa2
Paul Cohen <pacoispaco@gmail.com>
2017-09-24 23:17:15 +0200

    Add support for reading JSON files and for reading IOC files in correct order

--- src/ts-ioc.py
+++ src/ts-ioc.py
@@ -12,69 +12,87 @@ import copy
 
 import pprint
 
-# Constants
+# Error constants
 ERROR_FILE_NOT_FOUND = 1
 ERROR_FILE_NOT_EXCEL = 2
+# File type constants. The value also denotes the order in which these files
+# should be read
 TYPE_IOC_MASTER_FILE = 1
 TYPE_IOC_OTHER_LISTS_FILE = 2
 TYPE_IOC_MULTILINGUAL_FILE = 3
 TYPE_IOC_COMPLEMENTARY_FILE = 4
-TYPE_UNRECOGNIZED_FILE = 5
+TYPE_UNRECOGNIZED_EXCEL_FILE = 5
 file_types = {TYPE_IOC_MASTER_FILE: "IOC Master file",
               TYPE_IOC_OTHER_LISTS_FILE: "IOC Other lists file",
               TYPE_IOC_MULTILINGUAL_FILE: "IOC Multilingual file",
               TYPE_IOC_COMPLEMENTARY_FILE: "IOC Complementary file",
-              TYPE_UNRECOGNIZED_FILE: "Unrecognized file"}
-DEFAULT_WRITE_DIR = "./ts_ioc-data"
+              TYPE_UNRECOGNIZED_EXCEL_FILE: "Unrecognized Excel file"}
+DEFAULT_IOC_DIR = "./data-ioc"
 
 # Globals
 # Object containing representation of file contents
 file_contents = {}
 # Object containing info on file
-file_info = {'infraclass_count': 0,
-             'order_count': 0,
-             'family_count': 0,
-             'genus_count': 0,
-             'species_count': 0,
-             'subspecies_count': 0}
+file_stats = {'infraclass_count': 0,
+              'order_count': 0,
+              'family_count': 0,
+              'genus_count': 0,
+              'species_count': 0,
+              'subspecies_count': 0}
 # IOC taxonomy as parsed from the IOC Master file
-taxonomy = []
+master_taxonomy = []
 # IOC taxonomy as parsed from the IOC other lists file
 taxonomy_ol = []
+ioc_dir = DEFAULT_IOC_DIR
 
-def kind_of_ioc_file (workbook):
-    """Return a tuple containing of the type and version of IOC file."""
-    # We look at which worksheets the workbook contains and their names to
-    # deduce the type of IOC file.
-    if workbook.worksheets[0].title == "Master":
-        s = workbook.worksheets[0].cell (row=1, column=2).value
+def kind_of_ioc_file (filename):
+    """Return an object containing info on the name, type and version of IOC file."""
+    # We try to read the 'filename' as an Excel file and then look at which
+    # worksheets the workbook contains and their names to deduce the type of
+    # IOC file.
+    try:
+        wb = xlxs.load_workbook (filename)
+    except xlxs.InvalidFileException:
+        print ("Error: '%s' is not an Excel file (.xlxs)." % (ioc_file))
+        sys.exit (ERROR_FILE_NOT_EXCEL)
+    if wb.worksheets[0].title == "Master":
+        s = wb.worksheets[0].cell (row=1, column=2).value
         regexp = re.compile (".*\((.*)\).*")
         version = regexp.match (s).groups()[0]
-        return (TYPE_IOC_MASTER_FILE, version)
-    elif "vs_other_lists" in workbook.worksheets[0].title:
-        s = workbook.worksheets[0].cell (row=1, column=2).value
+        return {'filename': filename,
+                'type': TYPE_IOC_MASTER_FILE,
+                'version': version}
+    elif "vs_other_lists" in wb.worksheets[0].title:
+        s = wb.worksheets[0].cell (row=1, column=2).value
         regexp = re.compile (".*\(v (.*)\).*")
         version = regexp.match (s).groups()[0]
-        return (TYPE_IOC_OTHER_LISTS_FILE, version)
-    elif workbook.worksheets[0].title == "List" and workbook.worksheets[1].title == "Sources":
-        s = workbook.worksheets[1].cell (row=1, column=1).value
+        return {'filename': filename,
+                'type': TYPE_IOC_OTHER_LISTS_FILE,
+                'version': version}
+    elif wb.worksheets[0].title == "List" and wb.worksheets[1].title == "Sources":
+        s = wb.worksheets[1].cell (row=1, column=1).value
         regexp = re.compile ("[A-Za-z ]*(\d*\.\d*).*")
         version = regexp.match (s).groups()[0]
-        return (TYPE_IOC_MULTILINGUAL_FILE, version)
-    elif "IOC" in workbook.worksheets[0].title:
-        s = workbook.worksheets[0].title
+        return {'filename': filename,
+                'type': TYPE_IOC_MULTILINGUAL_FILE,
+                'version': version}
+    elif "IOC" in wb.worksheets[0].title:
+        s = wb.worksheets[0].title
         regexp = re.compile ("[A-Za-z ]*(\d*\.\d*).*")
         version = regexp.match (s).groups()[0]
-        return (TYPE_IOC_COMPLEMENTARY_FILE, version)
+        return {'filename': filename,
+                'type': TYPE_IOC_COMPLEMENTARY_FILE,
+                'version': version}
     else:
-        return (TYPE_UNRECOGNIZED_FILE, None)
+        return (TYPE_UNRECOGNIZED_EXCEL_FILE, None)
 
-def read_ioc_master_file (workbook):
+def read_ioc_master_file (filename):
     """Read the IOC Master file workbook."""
-    # Note that the global variable 'taxonomy' is a list that will maintain the
+    # Note that the global variable 'master_taxonomy' is a list that will maintain the
     # ordering of taxa in the IOC Master file.
-    global taxonomy
-    ws = workbook.worksheets[0]
+    global master_taxonomy
+    wb = xlxs.load_workbook (filename)
+    ws = wb.worksheets[0]
     i = 0
     for row in ws.iter_rows (min_row=5):
         infraclass = row[0].value
@@ -95,60 +113,43 @@ def read_ioc_master_file (workbook):
         if infraclass:
             taxon['rank'] = "Infraclass"
             taxon['name'] = infraclass
-            taxonomy.append (taxon)
-            file_info['infraclass_count'] += 1
+            master_taxonomy.append (taxon)
+            file_stats['infraclass_count'] += 1
         elif order:
             taxon['rank'] = "Order"
             taxon['name'] = order
-            taxon['supertaxon'] = taxonomy[-1]['name']
-            taxonomy[-1]['subtaxa'].append (taxon)
-            file_info['order_count'] += 1
+            taxon['supertaxon'] = master_taxonomy[-1]['name']
+            master_taxonomy[-1]['subtaxa'].append (taxon)
+            file_stats['order_count'] += 1
         elif family:
             taxon['rank'] = "Family"
             taxon['name'] = family
-            taxon['supertaxon'] = taxonomy[-1]['subtaxa'][-1]['name']
-            taxonomy[-1]['subtaxa'][-1]['subtaxa'].append (taxon)
-            file_info['family_count'] += 1
+            taxon['supertaxon'] = master_taxonomy[-1]['subtaxa'][-1]['name']
+            master_taxonomy[-1]['subtaxa'][-1]['subtaxa'].append (taxon)
+            file_stats['family_count'] += 1
         elif genus:
             taxon['rank'] = "Genus"
             taxon['name'] = genus.title ()
-            taxon['supertaxon'] = taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['name']
-            taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'].append (taxon)
-            file_info['genus_count'] += 1
+            taxon['supertaxon'] = master_taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['name']
+            master_taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'].append (taxon)
+            file_stats['genus_count'] += 1
         elif species:
             taxon['rank'] = "Species"
             taxon['name'] = species
-            taxon['supertaxon'] = taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['name']
+            taxon['supertaxon'] = master_taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['name']
             genus = taxon['supertaxon']
             taxon['binomial_name'] = genus + " " + species
-            taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'].append (taxon)
-            file_info['species_count'] += 1
+            master_taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'].append (taxon)
+            file_stats['species_count'] += 1
         elif subspecies:
             taxon['rank'] = "Subspecies"
             taxon['name'] = subspecies
-            taxon['supertaxon'] = taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['name']
-            genus = taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['name']
+            taxon['supertaxon'] = master_taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['name']
+            genus = master_taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['name']
             species = taxon['supertaxon']
             taxon['trinomial_name'] = genus + " " + species + " " + subspecies
-            taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'].append (taxon)
-            file_info['subspecies_count'] += 1
-
-def read_file (ioc_file, verbose):
-    """Read the IOC file."""
-    global file_contents, file_info
-    if verbose:
-        print ("Reading file '%s' ..." % (ioc_file))
-    file_info['name'] = ioc_file
-    try:
-        wb = xlxs.load_workbook (ioc_file)
-        # Check what kind of IOC file we have
-        file_info['type'], file_info['version'] = kind_of_ioc_file (wb)
-        if file_info['type'] == TYPE_IOC_MASTER_FILE:
-            read_ioc_master_file (wb)
-
-    except xlxs.InvalidFileException:
-        print ("Error: '%s' is not an Excel file (.xlxs)." % (ioc_file))
-        sys.exit (ERROR_FILE_NOT_EXCEL)
+            master_taxonomy[-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'][-1]['subtaxa'].append (taxon)
+            file_stats['subspecies_count'] += 1
 
 def write_taxon_to_file (directory, taxon):
     """Write the JSON representation of 'taxon' to file."""
@@ -160,49 +161,124 @@ def write_taxon_to_file (directory, taxon):
         fname = taxon['trinomial_name'].replace (" ", "_") + ".json"
     else:
         fname = taxon['name'] + ".json"
-    subtaxa_names = []
+    # We save the filenames of the subtaxa in the attribute 'subtaxa'
+    subtaxa_files = []
     for subtaxon in taxon['subtaxa']:
         if subtaxon['rank'] == "Species":
-            subtaxa_names.append (subtaxon['binomial_name'])
+            subtaxa_files.append (subtaxon['binomial_name'].replace (" ", "_") + ".json")
         elif subtaxon['rank'] == "Subspecies":
-            subtaxa_names.append (subtaxon['trinomial_name'])
+            subtaxa_files.append (subtaxon['trinomial_name'].replace (" ", "_") + ".json")
         else:
-            subtaxa_names.append (subtaxon['name'])
-    taxon['subtaxa'] = subtaxa_names
+            subtaxa_files.append (subtaxon['name'] + ".json")
+    taxon['subtaxa'] = subtaxa_files
     f = open (os.path.join (directory, fname), 'w')
     f.write (json.dumps (taxon))
     f.close ()
 
-def write_to_files(verbose):
-    """Print JSON representations to files."""
+def write_master_taxonomy_to_files (verbose):
+    """Write JSON representations of the taxa in the master taxonomy to files."""
     if verbose:
         print ("Writing to files ...")
-    if os.path.exists (DEFAULT_WRITE_DIR):
-        print ("AError: Directory '%s' already exists." % (DEFAULT_WRITE_DIR))
+    if os.path.exists (DEFAULT_IOC_DIR):
+        print ("Error: Directory '%s' already exists." % (DEFAULT_IOC_DIR))
     else:
-        os.makedirs(DEFAULT_WRITE_DIR)
-    for taxon in taxonomy:
-        write_taxon_to_file (DEFAULT_WRITE_DIR, taxon)
+        os.makedirs(DEFAULT_IOC_DIR)
+    for taxon in master_taxonomy:
+        write_taxon_to_file (DEFAULT_IOC_DIR, taxon)
+
+def load_ioc_subtaxa (taxon):
+    """Load the subtaxa for the given taxon."""
+    global master_taxonomy
+    i = 0
+    for name in taxon['subtaxa']:
+        f = open (os.path.join (ioc_dir, name))
+        t = json.load (f)
+        f.close ()
+        taxon['subtaxa'][i] = t
+        if t['rank'] == "Order":
+            file_stats['order_count'] += 1
+        elif t['rank'] == "Family":
+            file_stats['family_count'] += 1
+        elif t['rank'] == "Genus":
+            file_stats['genus_count'] += 1
+        elif t['rank'] == "Species":
+            file_stats['species_count'] += 1
+        elif t['rank'] == "Subspecies":
+            file_stats['subspecies_count'] += 1
+        load_ioc_subtaxa (t)
+        i += 1
+
+def load_ioc_taxonomy (verbose):
+    """Load IOC taxonomy from files."""
+    global master_taxonomy, master_taxonomy_index
+    # Read infraclasses:
+    p = os.popen ("grep -l '\"rank\": \"Infraclass\"' %s/*.json" % (ioc_dir))
+    filenames = p.read ().split ()
+    p.close ()
+    for fname in filenames:
+        f = open (fname)
+        taxon = json.load (f)
+        f.close ()
+        master_taxonomy.append (taxon)
+        load_ioc_subtaxa (taxon)
+        file_stats['infraclass_count'] += 1
 
 def print_to_stdout (verbose):
     """Print JSON representations to stdout."""
     if verbose:
         print ("Printing to stdout ...")
-    print (json.dumps (taxonomy, indent=2))
+    print (json.dumps (master_taxonomy, indent=2))
 
 def print_info (verbose):
-    """Print info on IOC file to stdout."""
+    """Print info on IOC taxonomy to stdout."""
     if verbose:
-        print ("Printing info on '%s' ..." %(file_info['name']))
-    print ("File: '%s'" % (file_info['name']))
-    print ("Type: %s" % (file_types[file_info['type']]))
-    print ("Version: %s" % (file_info['version']))
-    print ("Infraclass count: %d" % (file_info['infraclass_count']))
-    print ("Order count: %d" % (file_info['order_count']))
-    print ("Family count: %d" % (file_info['family_count']))
-    print ("Genus count: %d" % (file_info['genus_count']))
-    print ("Species count: %d" % (file_info['species_count']))
-    print ("Subspecies count: %d" % (file_info['subspecies_count']))
+        print ("Printing info on '%s' ..." %(file_stats['name']))
+    print ("Infraclass count: %d" % (file_stats['infraclass_count']))
+    print ("Order count: %d" % (file_stats['order_count']))
+    print ("Family count: %d" % (file_stats['family_count']))
+    print ("Genus count: %d" % (file_stats['genus_count']))
+    print ("Species count: %d" % (file_stats['species_count']))
+    print ("Subspecies count: %d" % (file_stats['subspecies_count']))
+
+def handle_files (file_names, write, info, verbose):
+    """Handle the IOC files. if 'write' then write data to files. If 'info'
+       then print information on the contents of the files. If 'verbose'
+       then print information on progress and what's happening."""
+    global file_stats
+    # First inspect the files and determine the order to handle them in.
+    if verbose:
+        print ("Inspecting files ...")
+    file_infos = []
+    for name in file_names:
+        file_infos.append (kind_of_ioc_file (name))
+    file_infos = sorted (file_infos, key=lambda file_info: file_info['type'])
+    # If we don't have a TYPE_IOC_MASTER_FILE and we don't have any written
+    # files to work with, we must abandon, since we don't have any master data.
+    if not file_infos or not file_infos[0]['type'] == TYPE_IOC_MASTER_FILE:
+        if not os.path.exists (ioc_dir):
+            print ("Error: No master data in '%s' and no IOC Master File to read." % (ioc_dir))
+            sys.exit (3)
+        elif verbose:
+            print ("Using existing master data in '%s' ..." % (ioc_dir))
+        load_ioc_taxonomy (verbose)
+    else:
+        # Read and handle the master file
+        if verbose:
+            print ("Handling the IOC Master file ...")
+        read_ioc_master_file (file_infos[0]['filename'])
+        if write:
+            write_master_taxonomy_to_files (verbose)
+    # Now read and handle the rest of the files in the correct order.
+    if verbose:
+        print ("Reading other IOC files ...")
+    for file_info in file_infos:
+        pass
+    if write:
+        pass
+    else:
+        print_to_stdout (verbose)
+    if info:
+        print_info (verbose)
 
 def main():
     parser = argparse.ArgumentParser (description='Read IOC World Bird List\
@@ -213,7 +289,7 @@ def main():
     parser.add_argument ('-v', '--verbose', action='store_true', default=False,
                          help="print info about what's going on [False]")
     parser.add_argument ('-i', '--info', action='store_true', default=False,
-                         help="print info about the IOC file [False]")
+                         help="print info about the IOC files [False]")
     parser.add_argument ('-w', '--write', action='store_true', default=False,
                          help="write to JSON files [False]")
     parser.add_argument ('ioc_files', nargs='*', help="IOC file(s) to read")
@@ -224,15 +300,7 @@ def main():
             print ("Error: File '%s' not found." % (file))
             sys.exit (ERROR_FILE_NOT_FOUND)
     # Then read the files in correct order
-    # files = sort_files_according_to_type (args_ioc_files) TBD!
-    for file in args.ioc_files:
-        read_file (file, args.verbose)
-        if args.info:
-            print_info (args.verbose)
-        elif args.write:
-            write_to_files (args.verbose)
-        else:
-            print_to_stdout (args.verbose)
+    handle_files (args.ioc_files, args.write, args.info, args.verbose)
 
 if __name__ == "__main__":
     main()
