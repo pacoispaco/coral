69a563d823aa9e6723bb5fdb6195ad27cc8b2411
Paul Cohen <pacoispaco@gmail.com>
2017-10-09 07:57:48 +0200

    Add support for reading IOC Other Lists files

--- src/iocfiles.py
+++ src/iocfiles.py
@@ -40,11 +40,11 @@ def sorted_ioc_files (filepaths):
         if wb.worksheets[0].title == "Master":
             result.append (IocMasterFile (wb, path))
         elif "vs_other_lists" in wb.worksheets[0].title:
-            result.append (IocOtherListsFile (wbi, path))
+            result.append (IocOtherListsFile (wb, path))
         elif wb.worksheets[0].title == "List" and wb.worksheets[1].title == "Sources":
-            result.append (IocMultilingualFile (wbi, path))
+            result.append (IocMultilingualFile (wb, path))
         elif "IOC" in wb.worksheets[0].title:
-            result.append (IocComplementaryFile (wbi, path))
+            result.append (IocComplementaryFile (wb, path))
         else:
             print ("Error: '%s' is not an recognized IOC file." % (path))
             raise UnrecognizedIocFile (path)
@@ -61,18 +61,6 @@ def sorted_ioc_files (filepaths):
     # Return the list of IOC files sorted by their 'order' attribute
     return sorted (result, key=lambda iocfile: iocfile.order)
 
-def is_ioc_master_file (filepath):
-    """Return 'True' if 'filepath' is a IOC Master file."""
-    assert os.path.isfile (filepath)
-    try:
-        wb = xlxs.load_workbook (filepath)
-    except xlxs.InvalidFileException:
-        return False
-    if wb.worksheets[0].title == "Master":
-        return True
-    else:
-        return False
-
 class IocMasterFile (object):
     """Represents a IOC Master file."""
 
@@ -82,7 +70,9 @@ class IocMasterFile (object):
         self.workbook = workbook
         self.path = path
         self.version = None
-        self.taxonomy = []
+        self.taxonomy = []        # This list contains the list of top level
+                                  # taxa. Each taxa has a 'subtaxa' attribute
+                                  # which is a list of taxa, etc.
         self.taxonomy_stats = {}
         if self.workbook.worksheets[0].title == "Master":
             s = self.workbook.worksheets[0].cell (row=1, column=2).value
@@ -170,8 +160,21 @@ class IocOtherListsFile (object):
         self.workbook = workbook
         self.path = path
         self.version = None
-        self.taxonomy = []
+        self.taxonomy = {}          # This object contains taxa indexed by their
+                                    # name (or binomial or trinomial name if
+                                    # applicable (for species and subspecies)
         self.taxonomy_stats = {}
+        self.lists = {'ioc_7_3': 'Gill, F & D Donsker (Eds). 2017. IOC World Bird List (v 7.3)',
+                      'clements_2016': 'Clements, J. F., T. S. Schulenberg, M. J. Iliff, D. Roberson, T. A. Fredericks, B. L. Sullivan, and C. L. Wood. 2016. The eBird/Clements checklist of birds of the world: v2016.',
+                      'hbwbl_2016': 'del Hoyo, J., N. J. Collar, D. A. Christie, A. Elliott,  L. D. C. Fishpool, P. Boesman & G. M. Kirwan. 2014-2016. HBW and BirdLife International Illustrated Checklist of the Birds of the World, Volume 1, 2, Lynx Edicions in association with BirdLife International, Barcelona, Spain and Cambridge, UK.',
+                      'hm4_4ed': 'Dickinson, E.C., J.V. Remsen Jr. & L. Christidis (Eds). 2013-2014. The Howard & Moore Complete Checklist of the Birds of the World. 4th. Edition, Vol. 1, 2, Aves Press, Eastbourne, U.K. Plus CD content: Tyrberg, T. & E.C. Dickinson Appendix 5: Extinct species. (Also includes Errata and Corrigenda to Volume 1 and List of Errata for Vol. 2 plus Corrigenda in respect of range statements and additional Errata from Vol. 1)',
+                      'hbw_2013': 'del Hoyo, J., A. Elliott, J. Sargatal & D. A. Christie (Eds). 1992-2013. Handbook of the Birds of the World, Lynx Edicions.',
+                      'peters_1986': 'Peters, J.L. et al. Check-list of Birds of the World, 1931-1986. Harvard University Press/Museum of Comparative Zoology.',
+                      'boyd_3_08': 'John H. Boyd III - TiF checklist, Version 3.08: May 1 2017 and updated July 12 2017',
+                      'hbwbl_2017': 'BirdLife International (2017) Handbook of the Birds of the World and BirdLife International digital checklist of the birds of the world. Version 9.1. Available at: http://datazone.birdlife.org/userfiles/file/Species/Taxonomy/BirdLife_Checklist_Version_91.zip [.xls zipped 1 MB].',
+                      'sibley_1993': 'Sibley, C. G. and B. L. Monroe. 1993. A Supplement to Distribution and Taxonomy of Birds of the World. Yale University Press, New Haven, Connecticut.',
+                      'ioc_7_2': 'Gill, F & D Donsker (Eds). 2017. IOC World Bird List (v 7.2)',
+                      'ioc_7_1': 'Gill, F & D Donsker (Eds). 2017. IOC World Bird List (v 7.1)'}
         if "vs_other_lists" in self.workbook.worksheets[0].title:
             s = self.workbook.worksheets[0].cell (row=1, column=2).value
             regexp = re.compile (".*\(v (.*)\).*")
@@ -191,14 +194,51 @@ class IocOtherListsFile (object):
                                'genus_count': 0,
                                'species_count': 0,
                                'subspecies_count': 0}
+        ws = self.workbook.worksheets[0]
+        for row in ws.iter_rows (min_row=2):
+            name = row[1].value
+            self.taxonomy[name] = {'seq_no': row[0].value,
+                                   'name': name,
+                                   'notes': row[2].value,
+                                   'iucn_red_list_category': row[27].value,
+                                   'lists': {'clements_2016': {'name': row[4].value,
+                                                               'group': row[3].value,
+                                                               'family': row[17].value},
+                                             'hbwbl_2016': {'name': row[5].value,
+                                                            'group': row[6].value,
+                                                            'family': row[18].value},
+                                             'hm4_4ed': {'name': row[8].value,
+                                                         'group': row[7].value,
+                                                         'family': row[19].value},
+                                             'hbw_2013': {'name': row[9].value,
+                                                          'group': None,
+                                                          'family': row[20].value},
+                                             'peters_1986': {'name': row[10].value,
+                                                             'group': None,
+                                                             'family': row[21].value},
+                                             'boyd': {'name': row[11].value,
+                                                      'group': None,
+                                                      'family': row[22].value},
+                                             'hbwbl_2017': {'name': row[12].value,
+                                                            'group': None,
+                                                            'family': row[23].value},
+                                             'sibley_1993': {'name': row[13].value,
+                                                             'group': None,
+                                                             'family': row[24].value},
+                                             'ioc_7_2': {'name': None,
+                                                         'group': None,
+                                                         'family': row[25].value},
+                                             'ioc_7_1': {'name': None,
+                                                         'group': None,
+                                                         'family': row[26].value}}}
 
 class IocMultilingualFile (object):
     """Represents a IOC Multilingual file."""
 
-    def __init__ (self, workbooki, path):
+    def __init__ (self, workbook, path):
         """Initialize with the Excel 'workbook'."""
         self.order = 3
-        self.workbook= workbook
+        self.workbook = workbook
         self.path = path
         self.version = None
         self.taxonomy = []
@@ -229,7 +269,7 @@ class IocComplementaryFile (object):
     def __init__ (self, workbook, path):
         """Initialize with the Excel 'workbook'."""
         self.order = 4
-        self.workbook= workbook
+        self.workbook = workbook
         self.path = path
         self.version = None
         self.taxonomy = []
